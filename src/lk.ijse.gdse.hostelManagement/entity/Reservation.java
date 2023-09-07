cf54b0da1ab37238917736b7ed22834af26a4e3c
ChAnu VithAnawasam <chanumadhuprasanna99@gmail.com>
2023-09-07 14:57:40 +0530

    entity classes created

---
+++ src/lk.ijse.gdse.hostelManagement/entity/Reservation.java
@@ -0,0 +1,35 @@
+package lk.hostelManagement.pos.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Reservation {
+    @Id
+    private String res_id;
+    private LocalDate date;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_Id", referencedColumnName = "Student_Id")
+    private Student student_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "room_type_id", referencedColumnName = "room_Type_id")
+    private Room room_type_id;
+
+    private String key_money;
+    private Double advance;
+    private String status;
+
+
+}
