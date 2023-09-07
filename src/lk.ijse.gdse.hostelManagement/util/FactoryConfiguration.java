ec5d7e49484972ec859b127131c308730e15b1e1
ChAnu VithAnawasam <chanumadhuprasanna99@gmail.com>
2023-09-07 15:46:03 +0530

    FactroyConfigaration,NotificationCotroller,UILoader classes added

---
+++ src/lk.ijse.gdse.hostelManagement/util/FactoryConfiguration.java
@@ -0,0 +1,36 @@
+package lk.hostelManagement.pos.util;
+
+import lk.hostelManagement.pos.entity.Login;
+import lk.hostelManagement.pos.entity.Reservation;
+import lk.hostelManagement.pos.entity.Room;
+import lk.hostelManagement.pos.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+
+        Configuration config = new Configuration()
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Room.class)
+                .addAnnotatedClass(Reservation.class)
+                .addAnnotatedClass(Login.class);
+        sessionFactory = config.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return factoryConfiguration == null ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
+
---
+++ src/lk.ijse.gdse.hostelManagement/view/tm/LoginTM.java
@@ -0,0 +1,24 @@
+package lk.hostelManagement.pos.view.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class LoginTM {
+    @Id
+    private String userID;
+    @Column
+    private String name;
+    private String address;
+    private String contact_no;
+    private String Password;
+    private String gender;
+
+}
